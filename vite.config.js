910999884985b450cdddf2d15a596e175df04f9b
Angela codes for fun <91817457+PlaygroundForAngela@users.noreply.github.com>
2025-10-05 23:32:53 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import path from "path";
+import { fileURLToPath } from "url";
+import process from "process";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_PATH ,
+    plugins: [react(), mkcert()],
+    server: {
+      open: true,
+      port: 5173,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@apis": path.resolve(__dirname, "./src/apis"),
+        "@stores": path.resolve(__dirname, "./src/stores"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+      },
+    },
+  };
+});
